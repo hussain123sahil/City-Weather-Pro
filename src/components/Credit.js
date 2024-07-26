2e629c2bafd8738eb930ceb00ffd3e8684bb6dd0
sahil <sahilhussain1231@gmail.com>
2024-07-26 16:27:14 +0530

    completed all codes

---
+++ src/components/Credit.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Credit() {
+  return (
+    <div class="credit">
+    <p>
+      Coded by
+      <a href="https://www.linkedin.com/in/sahil-hussain-9a092b224/"
+        > Sahil Hussain</a
+      >, <a href="https://github.com/hussain123sahil/City-Weather-Pro">Source Code on Github.</a> Deployed on Vercel
+    </p>
+  </div>
+  );
+}
